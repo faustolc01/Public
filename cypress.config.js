9a21c490f6dfb81308cd04652c1395a01dfd1292
Fausto Cavalcante <faustolc1@gmail.com>
2022-07-27 08:48:00 -0300

    Add Project

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
+  },
+});
